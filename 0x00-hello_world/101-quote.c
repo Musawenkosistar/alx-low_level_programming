a15746e351f76e331ce862012a731feb2998a00f
Musawenkosistar <musawenkosi2504@gmail.com>
2023-06-15 19:45:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry
- * Return: 1
+ * main- Entry point
+ *
+ * Description: A program that prints to standard error
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
-
-{ 
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
