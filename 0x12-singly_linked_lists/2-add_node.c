1b71c1d766c5fc30254d11ef27330e6876ed3ac2
Musawenkosistar <musawenkosi2504@gmail.com>
2023-07-28 08:16:26 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,50 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
+
 /**
- * add_node - add a new node to a list
-(* a blank line
+ * add_node - adds a new node at the beginning of a linked list
  *@head: the head of list
  *@str: the string to put in the new node
-* Description: add a new node to a list)?
-(* section header: the header of this function is lists.h)*
-* Return: the head of the list.
+ *
+ * Return: the head of the list.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *node;
-	char *_str;
+	list_t *new;
+	unsigned int len = 0;
 
-	if (str == NULL)
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
-	node = malloc(sizeof(list_t));
-	if (node == NULL)
-		return (NULL);
-	_str = strdup(str);
-	if (_str == NULL)
-	{
-		free(node);
-		return (NULL);
-	}
-	node->len = _strlen(_str);
-	node->str = _str;
-	node->next = *head;
-	*head = node;
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
 	return (*head);
-}
-/**
- * _strlen - this functions prints the lenght of a string
- (* a blank line
- *@s: string to print.
- * Description: this function prints the lenght of a string?
- (* section header: the header of this function is holberton.h)*
- * Return: retunrn the lenght in int
- */
-int _strlen(char *s)
-{
-	if (*s != '\0')
-	{
-		s++;
-		return (1 + _strlen(s));
-	}
-	else
-		return (0);
 }
