9abb0818957a28ac151494fb2744a9241cf525f8
Musawenkosistar <musawenkosi2504@gmail.com>
2023-07-28 08:25:06 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,12 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
+
 /**
  * add_node_end - add a new node to the end of a list
-(* a blank line
- *@head: the head of list
+ *@head: double pointer to the list_t list
  *@str: the string to put in the new node
-* Description: add a new node to the end of a list)?
-(* section header: the header of this function is lists.h)*
-* Return: the list
+ *
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -43,22 +44,4 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	return (tail);
 
-}
-/**
- * _strlen - this functions prints the lenght of a string
- (* a blank line
- *@s: string to print.
- * Description: this function prints the lenght of a string?
- (* section header: the header of this function is holberton.h)*
- * Return: retunrn the lenght in int
- */
-int _strlen(char *s)
-{
-	if (*s != '\0')
-	{
-		s++;
-		return (1 + _strlen(s));
-	}
-	else
-		return (0);
 }
