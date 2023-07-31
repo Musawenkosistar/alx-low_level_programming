a3cd42da89d30be5b297a229949ea81858d453e3
Musawenkosistar <musawenkosi2504@gmail.com>
2023-07-31 14:36:17 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,22 +2,30 @@
 #include "lists.h"
 
 /**
- * print_list - function that prints all the elements of a list_t list
+ * print_list - function that prints all the components of a list_t list
  * @h: Const pointer of structure list_t
- * Return: Number of nodes
+ * Return: Number of meetting point
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	int count = 0;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
-			printf("[%u] %s\n", h->len, h->str);
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
-		s++;
+		count++;
 	}
-	return (s);
+
+	return (count);
 }
