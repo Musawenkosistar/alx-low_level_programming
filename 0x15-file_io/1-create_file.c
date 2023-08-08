ae032353adb18dfc175c9549da189dfcd74ce39e
Musawenkosistar <musawenkosi2504@gmail.com>
2023-08-08 00:04:54 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	rd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	j = write(fd, text_content, len);
+	j = write(rd, text_content, len);
 
 	if (rd == -1 || j == -1)
 		return (-1);
