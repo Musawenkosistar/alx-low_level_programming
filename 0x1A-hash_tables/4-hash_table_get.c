e9f96d9773398225ebb90f0150585a99ceb9faa4
Musawenkosistar <musawenkosi2504@gmail.com>
2023-10-07 23:18:52 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,28 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - Retrieve the value associated with
- *                  a key in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to get the value of.
- *
- * Return: If the key cannot be matched - NULL.
- *         Otherwise - the value associated with key in ht.
- */
+* hash_table_get - gets the element at the hash table
+* @ht: hash table
+* @key: key
+* Return: an element
+*/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
-	unsigned long int index;
+	unsigned long int index = 0;
+	hash_node_t *temporal = NULL;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!key)
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
-
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-
-	return ((node == NULL) ? NULL : node->value);
+	if (ht)
+	{
+		index = key_index((const unsigned char *)key, ht->size);
+		temporal = ht->array[index];
+		while (temporal)
+		{
+			if (strcmp(temporal->key, key) == 0)
+				return (temporal->value);
+			temporal = temporal->next;
+		}
+	}
+	return (NULL);
 }
